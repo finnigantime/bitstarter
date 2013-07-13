34411cb0704352a74b7b9e5c8ad44fe8cfc3ffb2
Patrick Finnigan <patrick.k.finnigan@gmail.com>
2013-07-13 22:11:47 +0000

    modifying web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var file = fs.readFileSync("index.html");
+var strFile = file.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(strFile);
 });
 
 var port = process.env.PORT || 5000;
